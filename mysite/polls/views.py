8312ed2d7188571d92a88fb36eddd58d95f363cc
kim <kimkooyeon@gmail.com>
2020-03-23 22:24:09 +0800

    Kick start

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Working. Hello, world. You are at the polls\\views.py")
+    return HttpResponse("Hello, world. You are at the polls\\views.py")
