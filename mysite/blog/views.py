07a71733b86d237e429246c1042e20202039900a
kimkooyeon@gmail.com <kimkooyeon@gmail.com>
2020-03-27 13:30:33 +0800

    Kick Start

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
     return HttpResponse('You are at Blog Home')
+
+def about(request):
+    return HttpResponse('Blog About')
