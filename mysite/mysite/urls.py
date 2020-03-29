4e6f220893ffe7b43bf83b5fb9d5b1795bf66efd
kimkooyeon@gmail.com <kimkooyeon@gmail.com>
2020-03-29 22:23:51 +0800

    static and media url setting

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,9 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as users_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', users_views.register, name='register'),
@@ -27,4 +30,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('blog/', include('blog.urls')),
     path('polls/', include('polls.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
