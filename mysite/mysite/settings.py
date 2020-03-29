0981196cf2da26aa72eb80d944e16862b0586df1
kimkooyeon@gmail.com <kimkooyeon@gmail.com>
2020-03-29 22:39:46 +0800

    static and media url setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# STATIC_ROOT = "/var/www/django/test.com/static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATIC_ROOT = "/var/www/django/test.com/static"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
