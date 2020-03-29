725fd7e0926606a893f916a2df1d57f7a8912988
kimkooyeon@gmail.com <kimkooyeon@gmail.com>
2020-03-29 12:59:33 +0800

    Register form is working with custom form. Class UserRegisterFormCustom(UserCreationForm):

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from .forms import UserRegisterFormCustom
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterFormCustom(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
             return redirect('/')
     else:
-        form = UserCreationForm()
+        form = UserRegisterFormCustom()
 
     context = {
         'form': form,
