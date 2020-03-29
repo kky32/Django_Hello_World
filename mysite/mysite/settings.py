b90726c1844924325039f84e6428cef5e3d4a337
kimkooyeon@gmail.com <kimkooyeon@gmail.com>
2020-03-29 23:06:58 +0800

    static and media url setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 # STATIC_ROOT = "/var/www/django/test.com/static"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
