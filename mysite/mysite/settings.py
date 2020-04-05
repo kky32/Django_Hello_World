d8f64b2f4648e740d43831516c8629e0b65f4bd9
kimkooyeon@gmail.com <kimkooyeon@gmail.com>
2020-04-05 21:15:27 +0800

    Class based views. Password Reset

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -132,3 +132,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
+
+# EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+# print(EMAIL_HOST_USER)
+# print(EMAIL_HOST_PASSWORD)
